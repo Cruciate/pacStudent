36ecaa98b2d468d54afa8da5c27255f837b6ad3d
Cruciate <matt206690@gmail.com>
2022-09-24 19:58:43 +1000

    Began adding animations

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public AudioClip clip1;
+
+    public AudioClip clip2;
+    void Start()
+    {
+        GetComponent<AudioSource>().clip = clip1;
+        GetComponent<AudioSource>().Play();
+        StartCoroutine(playSound());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator playSound()
+    {
+        //GetComponent<AudioSource>().clip = clip1;
+        yield return new WaitForSeconds(1);
+        GetComponent<AudioSource>().clip = clip2;
+        GetComponent<AudioSource>().Play();
+    }
+}
